57c26c6508b177557cb6fc9e3fe13aaf05a20e62
Mehregan Abdi <mehreganabdix@gmail.com>
2023-09-19 11:35:37 +0330

    Loaning Out Has a Bug

--- ILIb1.1/Controllers/UserDashBoardController.cs
+++ ILIb1.1/Controllers/UserDashBoardController.cs
@@ -3,6 +3,7 @@ using ILIb1._1.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace ILIb1._1.Controllers
 {
@@ -28,6 +29,32 @@ namespace ILIb1._1.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        public async Task<IActionResult> Loan(int BookId)
+        {
 
+            if (User.Identity.IsAuthenticated)
+            {
+                var loan = new Loan()
+                {
+                    BookId = BookId,
+                    UserId = User.Identity.GetUserId(),
+                    LoanDate = DateTime.Now
+                  
+                };
+                var userloans = _loanRepository.GetByUserIdAsync(User.Identity.GetUserId()).Result;
+                foreach (var item in userloans)
+                {
+                    if (item.BookId == BookId) return RedirectToAction("Index", "Book");
+                }
+                var loanStatus =  _loanRepository.AddLoan(loan);
+                if (loanStatus)
+                {
+                    return RedirectToAction("Index", "Book");
+                }
+                return View("Error");
+
+            }
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
